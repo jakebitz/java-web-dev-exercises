9fad4cf22a48eb264f55ff94995817e60dc032f3
unknown <jacobbitters@gmail.com>
2022-08-28 22:46:10 -0500

    worked on excersise

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,41 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
+//        public Course(String topic,Teacher instructor,ArrayList enrolledStudents){
+//                this.topic = topic;
+//                this.instructor = instructor;
+//                this.enrolledStudents = enrolledStudents;
+//
+//        }
+//        public void setTopic(String aTopic) {
+//                this.topic = aTopic;
+//        }
+//
+//        public void setInstructor(Teacher aInstructor) {
+//                this.instructor = aInstructor;
+//        }
+//
+//        public void setEnrolledStudents(ArrayList<Student> aEnrolledStudents) {
+//                this.enrolledStudents = aEnrolledStudents;
+//        }
+//
+//        public String getTopic() {
+//                return topic;
+//        }
+//
+//        public String getInstructor() {
+//                return Teacher.getLastName();
+//        }
+//
+//        public String getEnrolledStudents() {
+//                return ;
+//        }
+//
+
+}
