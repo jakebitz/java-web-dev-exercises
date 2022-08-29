5d3bcd7326bd90e33e65efb0faedafce8fe16c58
unknown <jacobbitters@gmail.com>
2022-08-29 18:20:20 -0500

    Fixed course getters and setters

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -7,35 +7,34 @@ public class Course {
         private String topic;
         private Teacher instructor;
         private ArrayList<Student> enrolledStudents;
-//        public Course(String topic,Teacher instructor,ArrayList enrolledStudents){
-//                this.topic = topic;
-//                this.instructor = instructor;
-//                this.enrolledStudents = enrolledStudents;
-//
-//        }
-//        public void setTopic(String aTopic) {
-//                this.topic = aTopic;
-//        }
-//
-//        public void setInstructor(Teacher aInstructor) {
-//                this.instructor = aInstructor;
-//        }
-//
-//        public void setEnrolledStudents(ArrayList<Student> aEnrolledStudents) {
-//                this.enrolledStudents = aEnrolledStudents;
-//        }
-//
-//        public String getTopic() {
-//                return topic;
-//        }
-//
-//        public String getInstructor() {
-//                return Teacher.getLastName();
-//        }
-//
-//        public String getEnrolledStudents() {
-//                return ;
-//        }
-//
+        public Course(String topic,Teacher instructor,ArrayList enrolledStudents){
+                this.topic = topic;
+                this.instructor = instructor;
+                this.enrolledStudents = enrolledStudents;
 
+        }
+
+        public String getTopic() {
+                return topic;
+        }
+
+        public void setTopic(String topic) {
+                this.topic = topic;
+        }
+
+        public Teacher getInstructor() {
+                return instructor;
+        }
+
+        public void setInstructor(Teacher instructor) {
+                this.instructor = instructor;
+        }
+
+        public ArrayList<Student> getEnrolledStudents() {
+                return enrolledStudents;
+        }
+
+        public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+                this.enrolledStudents = enrolledStudents;
+        }
 }
